15cdf7c8500f6f317662bb9527e75cccc6d8a813
itsJonCena <Alex@MBP-de-Freddy.lan>
2016-02-23 10:59:17 -0600

    inicio
    inicio del proyecto

---
+++ src/sample/Proceso.java
@@ -0,0 +1,12 @@
+package sample;
+
+/**
+ * Created by Alex on 23/02/16.
+ */
+public class Proceso {
+    private String id;
+    private int tiempoLlegada;
+    private int tiempoProcesador;
+    private int prioridad;
+
+}
