66120fc6f4c233c98be32478d5db01273f0f3717
Jesus <jesusguillermomendez@gmail.com>
2016-02-29 21:03:43 -0600

    Agregados solo tiempo de ejecucion ....

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,7 +1,7 @@
 package sample;
-
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -9,13 +9,27 @@ import java.util.ResourceBundle;
 public class Controller implements Initializable {
 
 
+    @FXML
+    private TextField Nompro;
+    @FXML
+    private TextField TiempoLlegada;
+    @FXML
+    private TextField TiempoProcesador;
+    @FXML
+    private TextField Prioridad;
+   
+
     @Override
     public void initialize(URL arg, ResourceBundle rb) {
     }
 
     @FXML
-    public void AgregarProceso(){
+    public void AgregarProceso() throws Exception {
+
+
 
     }
+
+
 
 }
---
+++ src/sample/FCFS.java
@@ -0,0 +1,15 @@
+package sample;
+import java.util.*;
+/**
+ * Created by Jesus on 28/02/2016.
+ */
+public class FCFS {
+        public static void main(String[] args){
+            ArrayList<Integer> fcfs = new ArrayList<Integer>();
+
+
+        }
+
+
+
+}
